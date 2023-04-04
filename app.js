6d2ad7a2575af6f31fbd77570c094338940412f3
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-04-04 16:49:37 +0200

    Error with Spotify's Web API - Styling on hold

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
-// require spotify-web-api-node package here:
+// ------------- Spotify-web-api-node package here:  ------------- //
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
+// ------------- Spotify-api goes here  ------------- //
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
-// Our routes go here:
+// ------------- Retrieve an access token  ------------- //
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body.access_token))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+// ------------- Routs go here ------------- //
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.hbs"));
+app.get("/artist-search", (req, res) => {
+  spotifyApi
+    .searchArtists(/*'HERE GOES THE QUERY ARTIST'*/)
+    .then((data) => {
+      console.log("The received data from the API: ", data.body);
+      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch((err) => console.log("The search threw the following error: ", err));
+});
+
+// ------------- Search results ------------- //
+app.get("/artists-search-results", (req, res) =>
+  res.sendFile(__dirname + "/views/search-results.hbs")
+);
+app.get('/albums/:artistId', (req, res, next) => {
+  // .getArtistAlbums() code goes here
+});
+
+app.listen(3000, () => console.log("Spotify running on port 3000"));
